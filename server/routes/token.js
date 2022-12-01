60daea1bf2eff57d0b9059af01265878d0c38085
c-catanese <111301202+c-catanese@users.noreply.github.com>
2022-11-30 21:43:14 -0500

    swapping branches

--- server/routes/token.js
+++ server/routes/token.js
@@ -7,9 +7,9 @@ router.post(
   '/',
   (req, res) => {
     res
-      .header('Access-Control-Allow-Origin', '*')
+      .setHeader('Access-Control-Allow-Origin', '*')
       .status(200)
-      .send(`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.gitHubRedirectURL}?path=${'/'}&scope=user:email`);
+      .redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.gitHubRedirectURL}?path=${'/'}&scope=user:email`);
   }
 );
 
