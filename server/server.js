3c5ebc8e9066b68c5ba5896fcb2843479bb31f5d
c-catanese <111301202+c-catanese@users.noreply.github.com>
2022-11-30 21:32:11 -0500

    swap branch

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-require("dotenv").config();
-const bodyParser = require("body-parser");
-const cors = require("cors");
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const path = require("path");
-const express = require("express");
+// const path = require('path');
+const express = require('express');
 
 const app = express();
 const { PORT } = process.env;
-const { auth } = require("./models/userModels");
+const { auth } = require('./models/userModels');
 
 const oauthController = require('./controllers/oauthController');
 
@@ -21,29 +21,29 @@ app.use(cors());
 /**
  * require routers
  */
-const apiRouter = require("./routes/api");
-const userRouter = require("./routes/user");
+const apiRouter = require('./routes/api');
+const userRouter = require('./routes/user');
 // oauthController.getToken,
 app.get('/token', (req, res, next) => { console.log(req.query.code); return next(); }, oauthController.getToken, (req, res) => res.sendStatus(200));
 // route handler to respond with main app
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
-app.use("/user", userRouter);
+app.use('/user', userRouter);
 
-app.use("*", (req, res, next) => {
+app.use('*', (req, res, next) => {
   const errorObj = {
-    log: "Page not found",
+    log: 'Page not found',
     status: 404,
-    message: { err: "Error 404: Page not Found" },
+    message: { err: 'Error 404: Page not Found' },
   };
   next(errorObj);
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   const defaultErr = {
-    log: "Express error handler caught unknown middleware error",
+    log: 'Express error handler caught unknown middleware error',
     status: 400,
-    message: { err: "An error occurred" },
+    message: { err: 'An error occurred' },
   };
 
   const errorObj = Object.assign(defaultErr, err);
